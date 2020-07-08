9826622d3d4ac90d4d276648c46922e18ae2159f
TTUDhanushka <dhanushka.liyanage@ttu.ee>
2020-07-08 18:32:44 +0300

    new white dark correction script

---
+++ CalibrateWhiteDark.m
@@ -0,0 +1,10 @@
+function corrected_image = CalibrateWhiteDark(hsi_cube, simultaneous)
+
+    if (corrected_image)
+        corrected_image = 0;
+    else
+        corrected_image = 0;
+    end
+
+
+end
