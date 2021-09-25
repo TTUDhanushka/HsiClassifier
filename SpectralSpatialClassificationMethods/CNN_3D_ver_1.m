531f003718a77b04bb4b07496e925e371786e4d3
TTUDhanushka <dhanushka.liyanage@ttu.ee>
2021-09-25 15:25:24 +0300

    3D CNN

---
+++ SpectralSpatialClassificationMethods/CNN_3D_ver_1.m
@@ -0,0 +1,13 @@
+%%  3D CNN
+
+% This script is based on "Spectral-spatial classification of hyperspectral
+% imagery with 3D convolutional neural network" by Li, Ying et al.. the
+% article was  published in MDPI "http://www.mdpi.com/2072-4292/9/1/67"
+
+%% Read the dataset
+
+% Network design
+
+% Performance matrices
+
+% Results for the thesis
